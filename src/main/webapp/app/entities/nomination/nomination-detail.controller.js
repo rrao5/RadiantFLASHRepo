2c498f5f48432dea146a3a06fdfa800b1039b202
rrao5 <raghuveer.rao@gmail.com>
2016-09-05 12:20:10 -0400

    added rewards and reviews

--- src/main/webapp/app/entities/nomination/nomination-detail.controller.js
+++ src/main/webapp/app/entities/nomination/nomination-detail.controller.js
@@ -5,13 +5,24 @@
         .module('peersApp')
         .controller('NominationDetailController', NominationDetailController);
 
-    NominationDetailController.$inject = ['$scope', '$rootScope', '$stateParams', 'previousState', 'entity', 'Nomination', 'User'];
+    NominationDetailController.$inject = ['$scope', '$rootScope', '$stateParams', 'previousState', 'entity', 'Nomination', 'User', 'Principal'];
 
-    function NominationDetailController($scope, $rootScope, $stateParams, previousState, entity, Nomination, User) {
+    function NominationDetailController($scope, $rootScope, $stateParams, previousState, entity, Nomination, User, Principal) {
         var vm = this;
 
         vm.nomination = entity;
         vm.previousState = previousState.name;
+        vm.loggedInUser = null;
+
+
+        getAccount();
+
+        function getAccount() {
+            Principal.identity().then(function(account) {
+                vm.loggedInUser = account.firstName + " " + account.lastName;
+
+            });
+        }
 
         var unsubscribe = $rootScope.$on('peersApp:nominationUpdate', function(event, result) {
             vm.nomination = result;
